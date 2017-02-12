ca570b8bb3d4157aece8be929f821a93cdd6c5b9
xuyanwang <xuyan@wisc.edu>
2017-02-11 19:16:30 -0600

    Update GradeEstimator.java

--- GradeEstimator.java
+++ GradeEstimator.java
@@ -1,3 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016 
+// PROJECT:          (project name)
+// FILE:             (file name)
+//
+// TEAM:    (team number and team nickname)
+// Authors: (Be sure to check if programming teams are allowed)
+// Author1: (name1,email1,netID1,lecture number1)
+// Author2: (name2,email2,netID2,lecture number2)
+//
+// ---------------- OTHER ASSISTANCE CREDITS 
+// Persons: Identify persons by name, relationship to you, and email. 
+// Describe in detail the the ideas and help they provided. 
+// 
+// Online sources: avoid web searches to solve your problems, but if you do 
+// search, be sure to include Web URLs and description of 
+// of any information you find. 
+//////////////////////////// 80 columns wide //////////////////////////////////
 import java.io.*;
 import java.util.Scanner;
 
